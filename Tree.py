2b2578c54e28802b271198a7b0810aa2ba41c8b8
MeHetavi <136117921+MeHetavi@users.noreply.github.com>
2024-01-10 19:48:19 +0530

    Update Tree.py
    Added a method that creates a tree by user input of node values.

--- Tree.py
+++ Tree.py
@@ -3,32 +3,44 @@ class TreeNode:
       self.val = val
       self.left = left
       self.right = right
-class Main:
-  def buildTree(inorder : list[int], postorder:list[int]):
-    postorder = list(reversed(postorder))
-    root = TreeNode(val = postorder[0])
-    if len(inorder) == 1:
-        return root
-    else:
-        i = inorder.index(postorder[0])
-        left = inorder[:i]
-        right = inorder[i+1:]
-        if right != [] :
-            root.right = buildTree(right,postorder[1:len(right)+1])
-        if left != []: 
-            root.left = buildTree(left,postorder[len(right)+1:])    
-        return root
 
-    def buildTree(inorder: list[int],preorder: list[int]) :
-            root = TreeNode(val = preorder[0])
-            if len(inorder) == 1:
-                return root
-            else:
-                i = inorder.index(preorder[0])
-                left = inorder[:i]
-                right = inorder[i+1:]
-                if left != [] :
-                    root.left = buildTree(left,preorder[1:len(left)+1])
-                if right != []: 
-                    root.right = buildTree(right,preorder[len(left)+1:])    
-                return root
+def buildTree(inorder : list[int], postorder:list[int]):
+  postorder = list(reversed(postorder))
+  root = TreeNode(val = postorder[0])
+  if len(inorder) == 1:
+      return root
+  else:
+      i = inorder.index(postorder[0])
+      left = inorder[:i]
+      right = inorder[i+1:]
+      if right != [] :
+          root.right = buildTree(right,postorder[1:len(right)+1])
+      if left != []: 
+          root.left = buildTree(left,postorder[len(right)+1:])    
+      return root
+
+  def buildTree(inorder: list[int],preorder: list[int]) :
+          root = TreeNode(val = preorder[0])
+          if len(inorder) == 1:
+              return root
+          else:
+              i = inorder.index(preorder[0])
+              left = inorder[:i]
+              right = inorder[i+1:]
+              if left != [] :
+                  root.left = buildTree(left,preorder[1:len(left)+1])
+              if right != []: 
+                  root.right = buildTree(right,preorder[len(left)+1:])    
+              return root
+def createTree(temp):
+    root = TreeNode(temp)
+    
+    temp = int(input(f'Enter Left Node Value of {root.val} : '))
+    if temp != -1: # does not create a node  if user enters -1.
+        root.left = createTree(temp)
+    
+    temp = int(input(f'Enter Right Node Value of {root.val} : '))
+    if temp != -1: # does not create a node  if user enters -1.
+        root.right = createTree(temp)
+
+    return root
